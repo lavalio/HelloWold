9bd00903f9e7cf33d6bc2f76cfbd54aa49020c8a
lavalio <jz2326@gmail.com>
2018-10-30 11:33:55 -0400

    String Excersice

---
+++ StringEx-1.py
@@ -0,0 +1,15 @@
+first_name = "Brendan"
+last_name = "Wood"
+email = "bwood@champlain.college.qc.ca"
+number = 42
+
+result = first_name.upper() + " " + last_name.upper() + "; and my favourite" \
+    + "number is " + str(number) + "; email: " +email
+
+print(result)
+
+
+result = first_name.lower() + " " + last_name.upper() + "; and my favourite" \
+    + "number is " + str(number)
+
+print(result)
